c8af17788a205e7e1de59f9aaea88fa46a12431f
gitucha <briantablet23@gmail.com>
2025-10-09 12:08:51 +0300

    creating auth with jwt

---
+++ users/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+# Create your models here.
+
+class User(AbstractUser):
+    xp = models.IntegerField(default=0)
+    level = models.IntegerField(default=1)
+    badges = models.JSONField(default=list, blank=True)
+    avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
+
+    def __str__(self):
+        return self.username
